9fefdf24a167969295fc2ef33c258ff0bed8887f
Alvaro Gaona <alvgaona@gmail.com>
2026-01-06 00:12:06 +0100

    Add basic operations

---
+++ util.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include <assert.h>
+#include <stdio.h>
+
+#include "mat.h"
+
+void print_mat(Mat *mat) {
+  assert(mat != NULL);
+  assert(mat->data != NULL);
+
+  printf("[\n");
+  for (size_t i = 0; i < mat->rows; i++) {
+    printf(" ");
+    for (size_t j = 0; j < mat->cols; j++) {
+      printf("%zu", mat->data[i * mat->cols + j]);
+      if (j < mat->cols - 1) {
+        printf(" ");
+      }
+    }
+    if (i < mat->rows - 1) {
+      printf(";");
+    }
+    printf("\n");
+  }
+  printf("]\n");
+}
+
+size_t *flatten_mat(size_t *in, size_t rows, size_t cols) {
+  assert(in != NULL);
+  assert(rows > 0);
+  assert(cols > 0);
+  
+  size_t *out = calloc(rows * cols, sizeof(size_t));
+
+  for (size_t i = 0; i < rows; i++) {
+    for (size_t j = 0; j < cols; j++) {
+      out[i * cols + j] = in[i * cols + j];
+    }
+  }
+
+  return out;
+}
